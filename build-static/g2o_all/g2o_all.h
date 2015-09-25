473d30c93c2eaa8c88b9be5ee7b2e7b36dc2962f
flames85 <flames85@163.com>
2015-09-25 11:15:35 +0800

    add build-statis

---
+++ build-static/g2o_all/g2o_all.h
@@ -0,0 +1,13 @@
+//
+//  g2o_all.h
+//  g2o_all
+//
+//  Created by Shao.Admin on 15/9/25.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface g2o_all : NSObject
+
+@end
---
+++ build-static/g2o_all/g2o_all.m
@@ -0,0 +1,13 @@
+//
+//  g2o_all.m
+//  g2o_all
+//
+//  Created by Shao.Admin on 15/9/25.
+//
+//
+
+#import "g2o_all.h"
+
+@implementation g2o_all
+
+@end
